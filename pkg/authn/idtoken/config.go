a3bed7fc22df39e78a499875872c410a9a16bc15
Gabriel MABILLE <gabriel.mabille@grafana.com>
2024-02-07 15:45:58 +0100

    Register flags allowedAudiences

--- pkg/authn/idtoken/config.go
+++ pkg/authn/idtoken/config.go
@@ -1,6 +1,9 @@
 package idtoken
 
-import "flag"
+import (
+	"flag"
+	"strings"
+)
 
 type Config struct {
 	SigningKeyURL    string   `yaml:"signingKeysUrl"`
@@ -9,4 +12,14 @@ type Config struct {
 
 func (c *Config) RegisterFlags(prefix string, fs flag.FlagSet) {
 	fs.StringVar(&c.SigningKeyURL, prefix+".signing-keys-url", "", "usage")
+	registerAllowedAudiences(c, prefix, &fs)
+}
+
+func registerAllowedAudiences(cfg *Config, prefix string, f *flag.FlagSet) {
+	var allowedAudiences string
+	f.StringVar(&allowedAudiences,
+		prefix+".allowed-audiences",
+		"",
+		"Specifies a comma-separated list of allowed audiences.")
+	cfg.AllowedAudiences = strings.Split(allowedAudiences, ",")
 }
