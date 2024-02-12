1a1d81107997d59eb79cfefbbee86d1c82a3fbde
Victor Cinaglia <victor@grafana.com>
2024-02-12 10:05:12 -0300

    authn: fix allowed-audiences flag parsing

--- authn/config.go
+++ authn/config.go
@@ -10,10 +10,11 @@ type IDVerifierConfig struct {
 	AllowedAudiences []string `yaml:"allowedAudiences"`
 }
 
-func (c *IDVerifierConfig) RegisterFlags(prefix string, fs flag.FlagSet) {
+func (c *IDVerifierConfig) RegisterFlags(prefix string, fs *flag.FlagSet) {
 	fs.StringVar(&c.SigningKeyURL, prefix+".signing-keys-url", "", "URL to jwks endpoint")
 
-	var allowedAudiences string
-	fs.StringVar(&allowedAudiences, prefix+".allowed-audiences", "", "Specifies a comma-separated list of allowed audiences.")
-	c.AllowedAudiences = strings.Split(allowedAudiences, ",")
+	fs.Func(prefix+".allowed-audiences", "Specifies a comma-separated list of allowed audiences.", func(v string) error {
+		c.AllowedAudiences = strings.Split(v, ",")
+		return nil
+	})
 }
