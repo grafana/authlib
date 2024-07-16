478e1f530b768d92657938ea563390c9a85b54aa
gamab <gabriel.mabille@grafana.com>
2024-07-16 08:53:32 +0200

    Move options back to main file

--- authz/multitenant_client.go
+++ authz/multitenant_client.go
@@ -55,6 +55,25 @@ type LegacyClientImpl struct {
 }
 
 // -----
+// Options
+// -----
+
+func WithCacheMTCOption(cache cache.Cache) MultiTenantClientOption {
+	return func(c *LegacyClientImpl) error {
+		c.cache = cache
+		return nil
+	}
+}
+
+func WithGrpcClientMTCOptions(opts ...grpc.DialOption) MultiTenantClientOption {
+	return func(c *LegacyClientImpl) error {
+		var err error
+		c.clientV1, err = newGRPCClient(c.authCfg.remoteAddress, opts...)
+		return err
+	}
+}
+
+// -----
 // Initialization
 // -----
 
