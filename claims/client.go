19e8da110c574ae766efc9a80cfd050402b704c9
Ryan McKinley <ryantxu@gmail.com>
2024-08-05 13:11:55 +0300

    add client

--- claims/client.go
+++ claims/client.go
@@ -2,10 +2,15 @@ package claims
 
 import "context"
 
-// Needs to play nicely with:
+// AccessRequest describes the requested access.
+// This is designed bo to play nicely with the kubernetes authorization system:
 // https://github.com/kubernetes/kubernetes/blob/v1.30.3/staging/src/k8s.io/apiserver/pkg/authorization/authorizer/interfaces.go#L28
-
 type AccessRequest struct {
+	// The requested access verb.
+	// this includes get, list, watch, create, update, patch, delete, deletecollection, and proxy,
+	// or the lowercased HTTP verb associated with non-API requests (this includes get, put, post, patch, and delete)
+	Verb string
+
 	// API group (dashboards.grafana.app)
 	Group string
 
@@ -28,13 +33,13 @@ type AccessRequest struct {
 
 type AccessClient interface {
 	// HasAccess checks whether the user can perform the given action for all requests
-	HasAccess(ctx context.Context, id AuthInfo, verb string, req ...AccessRequest) (bool, error)
+	HasAccess(ctx context.Context, id AuthInfo, req AccessRequest) (bool, error)
 
 	// Compile generates a function to check whether the id has access to items matching a request
 	// This is particularly useful when you want to verify access to a list of resources.
 	// Returns nil if there is no access to any matching items
-	Compile(ctx context.Context, id AuthInfo, verb string, req AccessRequest) (AccessChecker, error)
+	Compile(ctx context.Context, id AuthInfo, req AccessRequest) (AccessChecker, error)
 }
 
-// Checks for access to a specific item
+// Checks access while iterating within a resource
 type AccessChecker func(namespace string, name string) bool
