b59fb7547450d07ca1e10ee7c0a15c5c190b69f1
Karl Persson <kalle.persson@grafana.com>
2024-09-26 14:42:44 +0200

    Adjust AccessClient interface

--- claims/access.go
+++ claims/access.go
@@ -2,10 +2,10 @@ package claims
 
 import "context"
 
-// AccessRequest describes the requested access.
+// CheckRequest describes the requested access.
 // This is designed bo to play nicely with the kubernetes authorization system:
 // https://github.com/kubernetes/kubernetes/blob/v1.30.3/staging/src/k8s.io/apiserver/pkg/authorization/authorizer/interfaces.go#L28
-type AccessRequest struct {
+type CheckRequest struct {
 	// The requested access verb.
 	// this includes get, list, watch, create, update, patch, delete, deletecollection, and proxy,
 	// or the lowercased HTTP verb associated with non-API requests (this includes get, put, post, patch, and delete)
@@ -31,15 +31,51 @@ type AccessRequest struct {
 	Path string
 }
 
-type AccessClient interface {
-	// HasAccess checks whether the user can perform the given action for all requests
-	HasAccess(ctx context.Context, id AuthInfo, req AccessRequest) (bool, error)
+type CheckResponse struct {
+	Allowed bool
+}
 
-	// Compile generates a function to check whether the id has access to items matching a request
-	// This is particularly useful when you want to verify access to a list of resources.
-	// Returns nil if there is no access to any matching items
-	Compile(ctx context.Context, id AuthInfo, req AccessRequest) (AccessChecker, error)
+type ListRequest struct {
+	// The requested access verb.
+	// this includes get, list, watch, create, update, patch, delete, deletecollection, and proxy,
+	// or the lowercased HTTP verb associated with non-API requests (this includes get, put, post, patch, and delete)
+	Verb string
+
+	// API group (dashboards.grafana.app)
+	Group string
+
+	// ~Kind eg dashboards
+	Resource string
+
+	// tenant isolation
+	Namespace string
+
+	// Optional subresource
+	Subresource string
+
+	// For non-resource requests, this will be the requested URL path
+	Path string
+}
+
+type ListResponse struct {
+	// If all is set we can access all resources
+	All bool
+	// If all is false this will be popublated with all names we can access
+	Items []string
 }
 
 // Checks access while iterating within a resource
 type AccessChecker func(namespace string, name string) bool
+
+type AccessClient interface {
+	// Check checks whether the identity can perform the given action for all requests
+	Check(ctx context.Context, id AuthInfo, req CheckRequest) (*CheckRequest, error)
+
+	// List returns a list of resource names that can be accessed for given action.
+	List(ctx context.Context, id AuthInfo, req ListRequest) (*ListResponse, error)
+
+	// Compile generates a function to check whether the id has access to items matching a request
+	// This is particularly useful when you want to verify access to a list of resources.
+	// Returns nil if there is no access to any matching items
+	Compile(ctx context.Context, id AuthInfo, req ListRequest) (AccessChecker, error)
+}
