e93c95fdc2f63d3ed549466dbfce797e8b06e813
Karl Persson <kalle.persson@grafana.com>
2024-12-19 10:31:02 +0100

    use key set in grpc authenticator

--- authn/authenticator.go
+++ authn/authenticator.go
@@ -71,7 +71,8 @@ func (p GRPCTokenProvider) AccessToken(_ context.Context) (string, bool) {
 }
 
 func (p GRPCTokenProvider) IDToken(_ context.Context) (string, bool) {
-	const key = "X-Grafana-Id"
+	// FIXME: we should use the same key as we do over http.
+	const key = DefaultIdTokenMetadataKey
 	values := p.md.Get(key)
 	if len(values) == 0 {
 		return "", false
