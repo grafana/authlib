86b4f0aff5bce587ae0737941faae72c5d061627
Ryan McKinley <ryantxu@gmail.com>
2025-01-19 18:01:57 +0300

    cleanup

--- authn/grpc_client_interceptors.go
+++ authn/grpc_client_interceptors.go
@@ -155,8 +155,6 @@ func (gci *GrpcClientInterceptor) wrapContext(ctx context.Context) (context.Cont
 		md = make(metadata.MD)
 	}
 
-	fmt.Printf("WRAP (before): %+v\n", md)
-
 	if gci.cfg.accessTokenAuthEnabled {
 		token, err := gci.tokenClient.Exchange(spanCtx, *gci.cfg.TokenRequest)
 		if err != nil {
@@ -192,6 +190,5 @@ func (gci *GrpcClientInterceptor) wrapContext(ctx context.Context) (context.Cont
 		span.SetAttributes(attribute.String("keys", strings.Join(keys, ",")))
 	}
 
-	fmt.Printf("WRAP (outgoing): %+v\n", md)
 	return metadata.NewOutgoingContext(ctx, md), nil
 }
