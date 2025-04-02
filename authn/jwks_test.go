8fc6d990664549515d64b5aaad4ad36a3a9bf0d2
Marcelo E. Magallon <marcelo.magallon@grafana.com>
2025-04-02 15:16:37 -0600

    Add WithCache options for NewKeyRetriever and NewTokenExchangeClient
    It might be worth offering the option to set a custom cache, possibly
    with different policies or with a different caching store.
    
    This is almost supported, as cache.Cache is an interface, so it's
    possible to reimplement it. This could be done using a
    DefaultKeyRetrieverOption function to pass to NewKeyRetriever that can
    replace the cache field `c`. The problem is that the field is not
    exported, so it's not possible to implement the option outside of the
    package, and the type does not offer a function to set the private field
    (such a function would make the option unnecessary). The same goes for
    the TokenExchangeClient.
    
    Add those options in this change. It needs to take into consideration
    the fact that cache.NewLocalCache ends up starting a goroutine to do
    clean up tasks. While the goroutine should stop because the cache has
    been replaced, it's better to not start it in the first place.
    
    Signed-off-by: Marcelo E. Magallon <marcelo.magallon@grafana.com>

--- authn/jwks_test.go
+++ authn/jwks_test.go
@@ -3,10 +3,13 @@ package authn
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/go-jose/go-jose/v3"
 	"github.com/stretchr/testify/assert"
@@ -35,6 +38,9 @@ func TestDefaultKeyRetriever_Get(t *testing.T) {
 	service := NewKeyRetriever(KeyRetrieverConfig{
 		SigningKeysURL: server.URL,
 	})
+
+	require.NotNil(t, service)
+	require.NotNil(t, service.c)
 
 	t.Run("should fetched key if not cached", func(t *testing.T) {
 		key, err := service.Get(context.Background(), firstKeyID)
@@ -65,4 +71,92 @@ func TestDefaultKeyRetriever_Get(t *testing.T) {
 			assert.Equal(t, calls, 2)
 		}
 	})
+}
+
+func TestWithKeyRetrieverCache(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write(keys())
+	}))
+
+	t.Cleanup(func() {
+		server.CloseClientConnections()
+		server.Close()
+	})
+
+	tc := &testCache{data: make(map[string][]byte)}
+
+	service := NewKeyRetriever(KeyRetrieverConfig{
+		SigningKeysURL: server.URL,
+	}, WithKeyRetrieverCache(tc))
+
+	require.NotNil(t, service, "service should not be nil")
+	require.NotNil(t, service.c, "there should be a cache")
+	require.Equal(t, tc, service.c, "the cache should be the one passed in the options")
+
+	// The cache is empty, so the implementation should fetch the key.
+	key, err := service.Get(context.Background(), firstKeyID)
+	require.NoError(t, err)
+	require.NotNil(t, key)
+	assert.Equal(t, firstKeyID, key.KeyID)
+
+	// If the implementation called the cache, the data should be there.
+	data, err := tc.Get(context.Background(), firstKeyID)
+	require.NoError(t, err)
+	require.NotNil(t, data)
+
+	// Remove the key from the cache; the implementation should still return the key.
+	err = tc.Delete(context.Background(), firstKeyID)
+	require.NoError(t, err)
+
+	key, err = service.Get(context.Background(), firstKeyID)
+	require.NoError(t, err)
+	require.NotNil(t, key)
+	assert.Equal(t, firstKeyID, key.KeyID)
+
+	// Retrieve an invalid key; the implementation should return an error.
+	key, err = service.Get(context.Background(), "invalid")
+	require.ErrorIs(t, err, ErrInvalidSigningKey)
+	require.Nil(t, key)
+
+	// The implementation adds invalid keys to the cache to prevent re-fetching.
+	data, err = tc.Get(context.Background(), "invalid")
+	require.NoError(t, err)
+	require.NotNil(t, data)
+	require.Empty(t, data)
+}
+
+type testCache struct {
+	mu   sync.Mutex
+	data map[string][]byte
+}
+
+func (cache *testCache) Get(ctx context.Context, key string) ([]byte, error) {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+
+	item, ok := cache.data[key]
+	if !ok {
+		return nil, errors.New("not found")
+	}
+
+	return item, nil
+}
+
+func (cache *testCache) Set(ctx context.Context, key string, value []byte, expire time.Duration) error {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+
+	cache.data[key] = value
+
+	return nil
+}
+
+func (cache *testCache) Delete(ctx context.Context, key string) error {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+
+	delete(cache.data, key)
+
+	return nil
 }
